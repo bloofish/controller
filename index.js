94c72801f1ff044c59e08ca27085ad3e15205a57
fraser <fmgeddes@gmail.com>
2022-01-24 12:23:03 +1300

    prettygoodjobsofar

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const axios = require("axios");
 const { WebSocket } = require("ws");
 require("dotenv").config();
-var Gpio = require("onoff").Gpio; //include onoff to interact with the GPIO
+const Gpio = require("onoff").Gpio; //include onoff to interact with the GPIO
 
 const API_URL = "https://francescogorini.com/rpi-relay";
 const SOCK_URL = "wss://francescogorini.com/rpi-relay";
 
-var LED = new Gpio(4, "out"); //use GPIO pin 4, and specify that it is output
+const Lforward = new Gpio(4, "out"); //use GPIO pin 4, and specify that it is output
+//const Rforward = new Gpio(1, "out");
+//const Lreverse = new Gpio(1, "out");
+//const Rreverse = new Gpio(1, "out");
+
+const timeOut = 200;
 
 initSocketConn = async () => {
   try {
@@ -22,6 +27,7 @@ initSocketConn = async () => {
 
     // Websocket events
     ws.on("open", () => {
+      const handle = setTimeout(() => clearInputs(), timeOut);
       console.log("Websocket connection established");
     });
 
@@ -30,7 +36,7 @@ initSocketConn = async () => {
     });
 
     ws.on("message", (data) => {
-      checkInput(data);
+      checkInputs(data);
       console.log(`Recieved from relay server: ${data.toString()}`);
     });
   } catch (err) {
@@ -38,19 +44,87 @@ initSocketConn = async () => {
   }
 };
 
+let wTimeOut = new Date().getTime();
+let sTimeOut = new Date().getTime();
+let aTimeOut = new Date().getTime();
+let dTimeOut = new Date().getTime();
+let wPressed = 0;
+let sPressed = 0;
+let aPressed = 0;
+let dPressed = 0;
+
 initSocketConn();
 //led.unexport();
 
-function checkInput(data) {
-  if (err) {
-    throw err;
+const clearInputs = () => {
+  if (now.getTime() - wTimeOut > timeOut) {
+    wPressed = 0;
   }
-  if (data.toString == "W") {
-    LED.writeSync(1);
-    console.log("on");
+  if (now.getTime() - sTimeOut > timeOut) {
+    sPressed = 0;
   }
-  if (data.toString == "S") {
-    LED.writeSync(0);
-    console.log("off");
+  if (now.getTime() - aTimeOut > timeOut) {
+    aPressed = 0;
   }
-}
+  if (now.getTime() - dTimeOut > timeOut) {
+    dPressed = 0;
+  }
+  setInputs();
+};
+const checkInputs = (data) => {
+  if (data.toString() == "W") {
+    wPressed = 1;
+    wTimeOut = new Date().getTime();
+  }
+  if (data.toString() == "S") {
+    sPressed = 1;
+    sTimeOut = new Date().getTime();
+  }
+  if (data.toString() == "A") {
+    aPressed = 1;
+    aTimeOut = new Date().getTime();
+  }
+  if (data.toString() == "D") {
+    dPressed = 1;
+    dTimeOut = new Date().getTime();
+  }
+  setInputs();
+};
+const setInputs = () => {
+  if (wPressed && !sPressed && !aPressed && !dPressed) {
+    //forward
+    Lforward.writeSync(1);
+  }
+  if (!wPressed && sPressed && !aPressed && !dPressed) {
+    //backward
+  }
+  if (!wPressed && !sPressed && aPressed && !dPressed) {
+    //left
+  }
+  if (!wPressed && !sPressed && !aPressed && dPressed) {
+    //right
+  }
+  if (wPressed && !sPressed && aPressed && !dPressed) {
+    //forward left
+  }
+  if (wPressed && !sPressed && !aPressed && dPressed) {
+    //forward right
+  }
+  if (!wPressed && sPressed && aPressed && !dPressed) {
+    //backward left
+  }
+  if (!wPressed && sPressed && !aPressed && dPressed) {
+    //backward right
+  }
+  if (!wPressed && !sPressed && !aPressed && !dPressed) {
+    //stopped
+    Lforward.writeSync(0);
+  }
+
+  if (aPressed && dPressed) {
+    //a&d stop turning (priority)
+  }
+  if (wPressed && sPressed) {
+    //s&d stop (priority)
+  }
+};
