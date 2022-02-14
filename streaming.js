56a41cba5cdbb37e89c6e35c5931bee195ddf699
Francesco <goras.francesco@gmail.com>
2022-02-15 00:57:51 +1300

    Removewd old code. Set CLI flags as constants to be tweaked. Improved logging

--- streaming.js
+++ streaming.js
@@ -1,26 +1,18 @@
 const { WebSocket } = require('ws');
 const websocketStream = require('websocket-stream');
-const { StreamCamera, Codec, Flip, SensorMode } = require('pi-camera-connect');
 const { spawn } = require('child_process');
 require('dotenv').config();
 
 // const API_URL = "https://francescogorini.com/rpi-relay";
 // const SOCK_URL = "wss://francescogorini.com/rpi-relay";
-
 const API_URL = "http://192.168.1.65:9999/rpi-relay";
 const SOCK_URL = "ws://192.168.1.65:9999/rpi-relay";
 
-// const FPS = 5;
-// const streamCamera = new StreamCamera({
-//   codec: Codec.H264,
-//   flip: Flip.Both,
-//   sensorMode: SensorMode.Mode7,
-//   fps: FPS // This doesn't seem to work
-
-// });
-// let lastFrameTime = Date.now()
-
-let vstream;
+const FPS = '20'
+const BitRate = '25000'
+const Width = '480'
+const Height = '360'
+const Rotation = '180'
 
 const InitStreamConn = async (token) => {
   try {
@@ -34,24 +26,15 @@ const InitStreamConn = async (token) => {
     streamSock.on("open", async () => {
       console.log("Stream Websocket connection established");
 
+      // Spawn child to fetch video stream
+      const vstream = spawn('raspivid', ['-t', '9999999', '-fps', FPS, '-b', BitRate, '-w', Width, '-h', Height, '-rot', Rotation, '-ih', '-o', '-', '-n']);
+      vstream.on('close', (code) => console.log(`videostream: process exited with code ${code}`));
 
-      vstream = spawn('raspivid', ['-t', '9999999', '-fps', '10', '-b', '20000', '-h', '300', '-w', '500', '-ro', '180', '-ih', '-o', '-', '-n']);
-      let streamSockVid = websocketStream(streamSock)
-      vstream.stdout.pipe(streamSockVid);
-      // vstream.stdout.on('data', async (data) => {
-      //   streamSock.send(data)
-      // })
+      // Pipe child's stdout to websocket
+      vstream.stdout.pipe(websocketStream(streamSock));
+      vstream.stderr.on('data', (data) => console.error(`videostream: stderr: ${data}`));
 
-      vstream.stderr.on('data', (data) => {
-        console.error(`videostream: stderr: ${data}`);
-      });
-
-      vstream.on('close', (code) => {
-        console.log(`videostream: process exited with code ${code}`);
-      });
-
-      // await streamCamera.startCapture();
-      console.log(`Camera is now capturing`);
+      console.log(`Camera is now capturing ${Width}x${Height}@${FPS}fps : bitrate=${BitRate}`);
     });
 
   } catch (err) {
